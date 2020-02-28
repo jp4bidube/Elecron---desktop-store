1543fde5da5a7e4b6c626f3f09067c9195a021cc
João Pedro <ninja321>
2020-02-28 18:13:59 -0300

    a

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -4,18 +4,23 @@ import "./styles.css";
 export default function ProductItem({ prod }) {
   return (
     <li className="dev-item">
-      <header>
-        {/* <img src={prod.avatar_url} alt={prod.name} /> */}
-        <div className="user-info">
-          <strong>{prod.name}</strong>
-          <span>{prod.quantity}</span>
-        </div>
-      </header>
-      <p>{prod.description}</p>
-      <p>{prod.value}</p>
-      {/* <a href={`https://github.com/${dev.github_username}`}>
+      <div>
+        <div className="containtItem">
+          <header>
+            {/* <img src={prod.avatar_url} alt={prod.name} /> */}
+            <div className="user-info">
+              <strong>{prod.name}</strong>
+              <span>{prod.quantity}</span>
+            </div>
+          </header>
+          <p>{prod.description}</p>
+          <p className="value">{prod.value}</p>
+          {/* <a href={`https://github.com/${dev.github_username}`}>
         Acessar perfil no Github
       </a> */}
+        </div>
+        <div className="actions"> teste</div>
+      </div>
     </li>
   );
 }
